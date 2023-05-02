d6237283b4f11f0b8703df65f39e8182a524dae4
Vedant Patil <pvedant2002@gmail.com>
2023-05-02 19:42:06 +0530

    ++

--- frontend/components/Doctor/DoctorButtons.js
+++ frontend/components/Doctor/DoctorButtons.js
@@ -1,11 +1,9 @@
 import useIsDoctorPending from "@/hooks/useIsDoctorPending";
-import useIsDoctorRegistered from "@/hooks/useIsDoctorRegistered";
 import useRegisterDoctorConfirm from "@/hooks/useRegisterDoctorConfirm";
 import { ActionIcon, Badge, Button, Group } from "@mantine/core";
 import { IconKey } from "@tabler/icons-react";
 
 export default function DoctorButtons({ access, address }) {
-  const { isDoctorRegistered } = useIsDoctorRegistered(address);
   const { isDoctorPending } = useIsDoctorPending(address);
   const { isLoading, isDoctor, registerDrConfirm } = useRegisterDoctorConfirm();
 
@@ -19,7 +17,7 @@ export default function DoctorButtons({ access, address }) {
 
       {access == 2 && (
         <>
-          {isDoctorRegistered && !isDoctorPending && !isDoctor && (
+          {!isDoctorPending && !isDoctor && (
             <Button onClick={registerDrConfirm} variant="subtle" disabled={isLoading} compact>
               <ActionIcon size="sm" color="blue">
                 <IconKey />
